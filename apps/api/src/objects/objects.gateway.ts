c2340b31faab92614fcbfcf9dbdd03c27052b7d8
Matrix <dev@heyama-test.local>
2026-09-03 09:39:26 +0000

    feat: implement Socket.IO gateway for real-time events

---
+++ apps/api/src/objects/objects.gateway.ts
@@ -0,0 +1,22 @@
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { ObjectResponseDto } from './dto/object-response.dto.js';
+
+@WebSocketGateway({
+  cors: {
+    origin: [process.env.FRONTEND_URL ?? 'http://localhost:3000', 'http://localhost:3000'],
+    credentials: true,
+  },
+})
+export class ObjectsGateway {
+  @WebSocketServer()
+  server: Server;
+
+  emitCreated(object: ObjectResponseDto) {
+    this.server.emit('object.created', object);
+  }
+
+  emitDeleted(id: string) {
+    this.server.emit('object.deleted', { id });
+  }
+}
